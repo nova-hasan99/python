3944c4ab71aaee622adb335454432111eb88e3ed
H A S A N <hasan25042019@gmail.com>
2024-09-26 23:05:59 +0600

    encapstsulatoin

---
+++ pre-recorded/module_6/encapsulation.py
@@ -0,0 +1,31 @@
+
+class bankAccount:
+    __balance = 100
+
+    # deposit
+    def deposit(self, amount = 0):
+        if amount > 0:
+            self.__balance += amount
+            print (f'{amount}tk Deposit Successfully')
+
+        else:
+            print (f'Invalid amount {amount}tk')
+
+    # withdraw
+    def withdraw(self, amount = 0):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            print (f'{amount}tk Withdraw Successfully')
+
+        else:
+            print ('insufficient Balance :)')
+
+    # check balance
+    def checkBalance(self):
+        print (f'Your Balance now {self.__balance}tk')
+
+balance = bankAccount()
+
+balance.deposit()
+balance.withdraw()
+balance.checkBalance()
