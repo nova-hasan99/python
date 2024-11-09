bebb74a8e8c908d005c34ececaff352d287684a6
H A S A N <hasan25042019@gmail.com>
2024-11-09 23:08:07 +0600

    fg

--- main.py
+++ main.py
@@ -34,5 +34,3 @@ for n in range(10):
 
 
 
-
-
--- test/test.py
+++ test/test.py
@@ -6,6 +6,7 @@ class myClass:
     def addTwo(self, a, b):
         z = self.x + b + self.y + a
         print(z)
+        
 
     def sum(self):
         self.addTwo(1, 2)
