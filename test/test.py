e82bed419140bcb45bb8830e9a48019ef9612707
H A S A N <hasan25042019@gmail.com>
2024-12-02 22:32:32 +0600

    python

--- test/test.py
+++ test/test.py
@@ -1,19 +1,4 @@
-
 class myClass:
-    x = 10
-    y = 5
+    x = 101
+    y = 20
 
-    def addTwo(self, a, b):
-        z = self.x + b + self.y + a
-        print(z)
-
-    def sum(self):
-        self.addTwo(1, 2)
-
-    def __init__(self, v):
-        z = self.x + self.y + v
-        print("const", z)
-
-obj = myClass(6)
-obj.addTwo(10, 5)
-obj.sum()
