6da541599a3a157b4e6b7d79ca1060c09c9d40ba
H A S A N <hasan25042019@gmail.com>
2024-12-30 18:22:39 +0600

    django

--- live/module_19/website/views.py
+++ live/module_19/website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html')
 
 def contact(request):
     return HttpResponse('Hello Contact')
