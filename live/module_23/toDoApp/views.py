c7900844eee29bc2c059f00cb2b4289e4f169380
H A S A N <hasan25042019@gmail.com>
2025-02-16 22:04:23 +0600

    initial commit

--- live/module_23/toDoApp/views.py
+++ live/module_23/toDoApp/views.py
@@ -16,7 +16,6 @@ def task_list(request):
     tasks = Task.objects.filter(user=request.user).order_by("-created_at")
     return render(request, 'todoapp/task_list.html', {'tasks' : tasks})
 
-
 @login_required
 def create_task(request):
     if request.method == 'POST':
@@ -45,6 +44,7 @@ def edit_task(request, pk):
 @login_required
 def delete_task(request, pk):
     task = get_object_or_404(Task, pk=pk, user=request.user)
+    
     
     if request.method == 'POST':
         task.delete()
