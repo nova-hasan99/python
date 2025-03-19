e92815827c2d84e4136bf69f6f77957b509b22af
H A S A N <hasan25042019@gmail.com>
2025-03-19 22:28:52 +0600

    index update

--- live/module_27/accounts/models.py
+++ live/module_27/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 class CustomUser(AbstractUser):
     email = models.CharField(unique=True)
     is_verified = models.BooleanField(default=False)
