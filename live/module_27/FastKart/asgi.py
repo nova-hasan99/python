f2a05adf029bbd974d59b42a6950b3b8be01e548
H A S A N <hasan25042019@gmail.com>
2025-03-25 22:59:30 +0600

    os import

--- live/module_27/FastKart/asgi.py
+++ live/module_27/FastKart/asgi.py
@@ -6,6 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
+# for file upload read and  write
 import os
 from django.core.asgi import get_asgi_application
 
--- live/module_27/FastKart/settings.py
+++ live/module_27/FastKart/settings.py
@@ -126,7 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 MEDIA_URL = '/media/'        # image save in 'media' folder
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   # image open following this path
 
