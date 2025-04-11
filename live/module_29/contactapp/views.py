86080181fcc1cb638703fced7f21cbc3b975783d
H A S A N <hasan25042019@gmail.com>
2025-04-11 23:13:44 +0600

    space

--- live/module_29/contactapp/views.py
+++ live/module_29/contactapp/views.py
@@ -19,7 +19,6 @@ class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     permission_classes = [IsAuthenticated]
 
-
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
