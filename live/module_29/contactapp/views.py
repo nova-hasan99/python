22c19663ab9bf0a3fde4530a05ff092a1c40e384
H A S A N <hasan25042019@gmail.com>
2025-04-13 11:57:23 +0600

    pagination & ordaring

--- live/module_29/contactapp/views.py
+++ live/module_29/contactapp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import FilterSet,  NumberFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Contact, Author, Book
 from .serializers import ContactSerializer, AuthorSerializer, BookSerializer
+
+from rest_framework.pagination import PageNumberPagination
+class MyPagination(PageNumberPagination):
+    page_size = 2
 
 
 class ContactViewSet(viewsets.ModelViewSet):
@@ -22,12 +26,18 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["author", "title"]   # disable if use filterset_class
     # filterset_class = BookFilter   # enable if use BookFilter class
 
+    # filter_backends = [OrderingFilter]
+    # ordering_fields = ["author", "title"]
+
     # filter_backends = [SearchFilter]   # for search (search and filter not work together)
     # search_fields = ["title"]
+
+    #.............................Pagination
+    pagination_class = MyPagination
 
 
 # class BookFilter(FilterSet):    # filtaring book based on price
