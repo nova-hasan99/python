993577d6934d180a4638ac0cf4b3e19b244a46b5
H A S A N <hasan25042019@gmail.com>
2025-04-16 19:38:09 +0600

    pagination

--- live/module_29/contactapp/views.py
+++ live/module_29/contactapp/views.py
@@ -11,7 +11,6 @@ from rest_framework.pagination import PageNumberPagination
 class MyPagination(PageNumberPagination):
     page_size = 2
 
-
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
