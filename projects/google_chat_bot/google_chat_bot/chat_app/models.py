cd2f2d2ae60fb6bc354e66ce9a0b7f34d2e19286
H A S A N <hasan25042019@gmail.com>
2025-11-29 22:32:15 +0600

    add function

--- projects/google_chat_bot/google_chat_bot/chat_app/models.py
+++ projects/google_chat_bot/google_chat_bot/chat_app/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+# Create your models here.
+# class ChatMessage(models.Model):
+#     user = models.CharField(max_length=100)
+#     message = models.TextField()
+#     timestamp = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.user}: {self.message[:20]}..."
