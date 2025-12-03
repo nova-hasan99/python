9caae6d9351fa3e2080cf8dbe6a3aa315c22d62e
H A S A N <hasan25042019@gmail.com>
2025-12-03 22:10:46 +0600

    function remove

--- projects/google_chat_bot/google_chat_bot/google_chat_bot/wsgi.py
+++ projects/google_chat_bot/google_chat_bot/google_chat_bot/wsgi.py
@@ -13,3 +13,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'google_chat_bot.settings')
 
 application = get_wsgi_application()
+
+# pylint: disable=wrong-import-position
