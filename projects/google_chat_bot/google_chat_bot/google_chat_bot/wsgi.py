a556808b1167e4958988b968833fba8e9008e472
H A S A N <hasan25042019@gmail.com>
2025-12-07 21:52:30 +0600

    settings.py

--- projects/google_chat_bot/google_chat_bot/google_chat_bot/wsgi.py
+++ projects/google_chat_bot/google_chat_bot/google_chat_bot/wsgi.py
@@ -13,5 +13,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'google_chat_bot.settings')
 
 application = get_wsgi_application()
-
-# pylint: disable=wrong-import-position
